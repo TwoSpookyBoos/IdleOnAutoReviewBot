45edc5152c14d8553e9fa1c63cd4d53a992a4357
Niko Brajdić <niko.brajdic@diversys.solutions>
2024-01-16 21:33:13 +0100

    added logging config

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,8 +1,16 @@
 import json
+import sys
 from json import JSONDecodeError
-
+import coloredlogs
 from flask import Flask, render_template, request, url_for, redirect, Response
 import idleonTaskSuggester
+
+coloredlogs.DEFAULT_FIELD_STYLES['levelname']['color'] = 'white'
+coloredlogs.install(
+    level='DEBUG',
+    fmt='[%(levelname)s] %(asctime)s | %(name)s | %(module)s.%(funcName)s:%(lineno)d ~ %(message)s',
+    stream=sys.stdout,
+)
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -40,7 +48,7 @@ def index(main_or_beta: str) -> Response | str:
 
     try:
         capturedCharacterInput: str | dict = get_character_input()
-        # print("FlaskApp.index~ OUTPUT request.args.get('player'):",type(capturedCharacterInput),capturedCharacterInput)
+        app.logger.info("request.args.get('player'): %s %s", type(capturedCharacterInput), capturedCharacterInput)
         if request.method == 'POST' and isinstance(capturedCharacterInput, str):
             return redirect(url_for('index', player=capturedCharacterInput))
 
@@ -48,14 +56,14 @@ def index(main_or_beta: str) -> Response | str:
             pythonOutput = autoReviewBot(capturedCharacterInput)
 
     except Exception as reason:
-        print("FlaskApp.index~ Could not get Player from Request Args:", reason)
+        app.logger.error('Could not get Player from Request Args: %s', reason)
         error = True
 
     return render_template(page, htmlInput=pythonOutput, error=error, beta=main_or_beta)
 
 
 # @app.route("/")
-def autoReviewBot(capturedCharacterInput):
+def autoReviewBot(capturedCharacterInput) -> list | None:
     reviewInfo: list | None = None
     if capturedCharacterInput:
         reviewInfo = idleonTaskSuggester.main(capturedCharacterInput)
