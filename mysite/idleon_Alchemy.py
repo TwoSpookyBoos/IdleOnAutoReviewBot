7435fcdc68df92a56b5d8ac507d3c7b7abb462b3
TwoSpookyBoos <twospookyboos@gmail.com>
2024-02-26 19:10:37 -0600

    Copying Alchemy Bugfix from redesign

--- mysite/idleon_Alchemy.py
+++ mysite/idleon_Alchemy.py
@@ -688,7 +688,7 @@ def setAlchemyBubblesProgressionTier(inputJSON, progressionTiers, characterDict)
         if int(bubble) <= currentlyAvailableBubblesIndex:
             named_all_alchemyBubblesDict[getReadableBubbleNames(bubble, "Orange")] = int(raw_orange_alchemyBubblesDict[bubble])
     #print(named_all_alchemyBubblesDict)
-    for bubble in raw_orange_alchemyBubblesDict:
+    for bubble in raw_green_alchemyBubblesDict:
         if int(bubble) <= currentlyAvailableBubblesIndex:
             named_all_alchemyBubblesDict[getReadableBubbleNames(bubble, "Green")] = int(raw_green_alchemyBubblesDict[bubble])
     #print(named_all_alchemyBubblesDict)
