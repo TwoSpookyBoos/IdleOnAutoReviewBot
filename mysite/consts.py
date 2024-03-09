bd7490280910f887e89a173db55ed49c482434bd
TwoSpookyBoos <twospookyboos@gmail.com>
2024-03-08 23:37:58 -0600

    Update consts.py

--- mysite/consts.py
+++ mysite/consts.py
@@ -755,7 +755,7 @@ expectedStackables = {
         "EquipmentStatues7", "EquipmentStatues3", "EquipmentStatues2", "EquipmentStatues4", "EquipmentStatues14",  # Standard statues
         "EquipmentStatues1", "EquipmentStatues5",  # Plausible but time consuming
         "rtt0", "StoneZ1", "StoneT1", "StoneW1", "StoneA1",  #W1 Slow drops = Town TP + Stones
-        "StoneT2", "StoneA2", "StoneZ2",  #"StoneW2",  # W2 upgrade stones and Mystery2
+        "StoneT2", "StoneZ2",  "StoneW2",  #"StoneA2", # W2 upgrade stones and Mystery2
         "PureWater",  #W3 Slow drops = Distilled Water
         "FoodG9",  #W5 Slow drops = Golden W5 Sammy
     ],
