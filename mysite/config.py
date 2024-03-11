ccfd0cc3fb5d199068d7ec818f6eb92b552f8130
Niko Brajdić <niko.brajdic@diversys.solutions>
2024-03-11 18:26:14 +0100

    move all css to scss and auto-generate css on server start

--- mysite/config.py
+++ mysite/config.py
@@ -1,4 +1,5 @@
 import os
+import sass
 
 from flask import Flask
 from pathlib import Path
@@ -8,3 +9,9 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["PROJECT_ROOT"] = Path(os.path.dirname(os.path.abspath(__file__)))
 app.config["LOGS"] = app.config["PROJECT_ROOT"] / "logs"
+
+static_dir = app.config["PROJECT_ROOT"] / app.static_folder
+css_files_dir = static_dir / "styles"
+sass_files_dir = static_dir / "assets"
+
+sass.compile(dirname=(sass_files_dir, css_files_dir))
