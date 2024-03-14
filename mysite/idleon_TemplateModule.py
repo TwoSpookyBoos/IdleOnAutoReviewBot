a4529d7541315b26cac5e73d398f159df1188072
TwoSpookyBoos <twospookyboos@gmail.com>
2024-03-13 20:43:51 -0500

    Update idleon_TemplateModule.py
    Changed template to use new AdviceSection style

--- mysite/idleon_TemplateModule.py
+++ mysite/idleon_TemplateModule.py
@@ -1,15 +1,30 @@
 import json
-import progressionResults
+from models import AdviceSection, AdviceGroup, Advice
+from utils import pl, get_logger
+from flask import g as session_data
+from consts import maxTiersPerGroup, progressionTiers
 
-def parseJSONtoLists(inputJSON):
-    templateList = json.loads(inputJSON["Template"])
+logger = get_logger(__name__)
+
+def parseJSONtoLists():
+    templateList = json.loads(session_data.account.raw_data["Template"])
     return templateList
 
-def setTemplateProgressionTier(inputJSON, progressionTiers):
+def setTemplateProgressionTier():
+    template_AdviceDict = {
+    }
+    template_AdviceGroupDict = {}
+    template_AdviceSection = AdviceSection(
+        name="Template",
+        tier="Not Yet Evaluated",
+        header="Best Template tier met: Not Yet Evaluated",
+        picture=""
+    )
+    highestTemplateSkillLevel = max(session_data.account.all_skills["TemplateSkill"])
+    if highestTemplateSkillLevel < 1:
+        template_AdviceSection.header = "Come back after unlocking Template!"
+        return template_AdviceSection
+
     tier_Template = 0
-    overall_TemplateTier = 0
-    advice_Template1 = ""
-    overall_TemplateTier = min(progressionTiers[-1][0], tier_Template)
-    advice_TemplateCombined = ["Best Template tier met: " + str(overall_TemplateTier) + "/" + str(progressionTiers[-1][0]), advice_Template1]
-    templatePR = progressionResults.progressionResults(overall_TemplateTier,advice_TemplateCombined,"")
-    return templatePR
+    max_tier = progressionTiers["Template"][-1][0]
+    overall_TemplateTier = min(max_tier, tier_Template)
