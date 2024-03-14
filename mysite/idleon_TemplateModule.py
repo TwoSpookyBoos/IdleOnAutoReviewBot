b1544ab1b24e63ceb7d50aa76cde5ed669aeb90c
TwoSpookyBoos <twospookyboos@gmail.com>
2024-03-13 20:29:14 -0500

    Revert "Update idleon_TemplateModule.py"
    This reverts commit 537fd95bb41c8d1acbfdc955c1d052bcd04f9950.

--- mysite/idleon_TemplateModule.py
+++ mysite/idleon_TemplateModule.py
@@ -1,31 +1,13 @@
 import json
-from models import AdviceSection, AdviceGroup, Advice
-from utils import pl, get_logger
-from flask import g as session_data
-from consts import maxTiersPerGroup, progressionTiers
 
-logger = get_logger(__name__)
-
-def parseJSONtoLists():
-    templateList = json.loads(session_data.account.raw_data["Template"])
+def parseJSONtoLists(inputJSON):
+    templateList = json.loads(inputJSON["Template"])
     return templateList
 
-def setTemplateProgressionTier():
-    template_AdviceDict = {
-    }
-    template_AdviceGroupDict = {}
-    template_AdviceSection = AdviceSection(
-        name="Template",
-        tier="Not Yet Evaluated",
-        header="Best Template tier met: Not Yet Evaluated",
-        picture=""
-    )
-    highestTemplateSkillLevel = max(session_data.account.all_skills["TemplateSkill"])
-    if highestTemplateSkillLevel < 1:
-        template_AdviceSection.header = "Come back after unlocking Template!"
-        return template_AdviceSection
-
+def setTemplateProgressionTier(inputJSON, progressionTiers):
     tier_Template = 0
-    max_tier = progressionTiers["Template"][-1][0]
-    overall_TemplateTier = min(max_tier, tier_Template)
+    overall_TemplateTier = 0
+    advice_Template1 = ""
+    overall_TemplateTier = min(progressionTiers[-1][0], tier_Template)
+    advice_TemplateCombined = ["Best Template tier met: " + str(overall_TemplateTier) + "/" + str(progressionTiers[-1][0]), advice_Template1]
 
