ae54860b3d35aeeb6f88e65b84ee1f2e4f099a69
TwoSpookyBoos <twospookyboos@gmail.com>
2024-05-16 10:33:25 -0500

    Add Vendors and Anvil to slab

--- mysite/w5/slab.py
+++ mysite/w5/slab.py
@@ -2,14 +2,16 @@ from models.models import AdviceSection, AdviceGroup, Advice
 from utils.text_formatting import pl, getItemDisplayName
 from utils.logging import get_logger
 from flask import g as session_data
-from consts import slabList, reclaimableQuestItems
+from consts import slabList, reclaimableQuestItems, vendorItems, anvilItems
 
 logger = get_logger(__name__)
 
 def setSlabProgressionTier():
     slab_AdviceDict = {
         "Reclaims": [],
-        "Storage": []
+        "Storage": [],
+        "Vendors": {},
+        "Anvil": {}
     }
     slab_AdviceGroupDict = {}
     slab_AdviceSection = AdviceSection(
@@ -39,7 +41,8 @@ def setSlabProgressionTier():
                 slab_AdviceDict["Storage"].append(Advice(
                     label=getItemDisplayName(itemName),
                     picture_class=getItemDisplayName(itemName) if itemName not in itemNameFindList else itemNameReplacementList[itemNameFindList.index(itemName)]))
-            elif itemName in reclaimableQuestItems.keys():
+                break
+            if itemName in reclaimableQuestItems.keys():
                 for characterIndex, characterQuests in enumerate(session_data.account.all_quests):
                     if reclaimableQuestItems[itemName].get("QuestNameCoded") in characterQuests:
                         if characterQuests[reclaimableQuestItems[itemName].get("QuestNameCoded")] > 0:
@@ -47,6 +50,31 @@ def setSlabProgressionTier():
                                 label=f"{getItemDisplayName(itemName)} ({reclaimableQuestItems[itemName].get('QuestGiver')}: {reclaimableQuestItems[itemName].get('QuestName')})",
                                 picture_class=getItemDisplayName(itemName) if itemName not in itemNameFindList else itemNameReplacementList[itemNameFindList.index(itemName)]))
                             break  #Only show this once per account, not once per character
+            for vendor, vendorList in vendorItems.items():
+                # If I search for the item first, these can appear out of order
+                if vendor not in slab_AdviceDict["Vendors"]:
+                    slab_AdviceDict["Vendors"][vendor] = []
+                if itemName in vendorList:
+                    slab_AdviceDict["Vendors"][vendor].append(Advice(
+                        label=getItemDisplayName(itemName),
+                        picture_class=getItemDisplayName(itemName) if itemName not in itemNameFindList else itemNameReplacementList[itemNameFindList.index(itemName)]))
+                    break
+            for anvilTab, anvilTabList in anvilItems.items():
+                # If I search for the item first, these can appear out of order
+                if anvilTab not in slab_AdviceDict["Anvil"]:
+                    slab_AdviceDict["Anvil"][anvilTab] = []
+                if itemName in anvilTabList:
+                    slab_AdviceDict["Anvil"][anvilTab].append(Advice(
+                        label=getItemDisplayName(itemName),
+                        picture_class=getItemDisplayName(itemName) if itemName not in itemNameFindList else itemNameReplacementList[itemNameFindList.index(itemName)]))
+                    break
+    #Remove any empty subgroups. Caused by creating the subgroups in order to preserve order.
+    for subgroup in slab_AdviceDict["Vendors"]:
+        if len(subgroup) == 0:
+            slab_AdviceDict["Vendors"].pop(subgroup)
+    for subgroup in slab_AdviceDict["Anvil"]:
+        if len(subgroup) == 0:
+            slab_AdviceDict["Anvil"].pop(subgroup)
     # Generate AdviceGroups
     slab_AdviceGroupDict["Storage"] = AdviceGroup(
         tier='',
@@ -55,8 +83,18 @@ def setSlabProgressionTier():
     )
     slab_AdviceGroupDict["Reclaims"] = AdviceGroup(
         tier='',
-        pre_string=f"The following item{pl(slab_AdviceDict['Storage'])} could be reclaimed from a quest and are not registered in The Slab",
+        pre_string=f"The following item{pl(slab_AdviceDict['Reclaims'])} could be reclaimed from a quest and are not registered in The Slab",
         advices=slab_AdviceDict["Reclaims"]
+    )
+    slab_AdviceGroupDict["Vendors"] = AdviceGroup(
+        tier='',
+        pre_string=f"The following item{pl(slab_AdviceDict['Vendors'])} could be purchased from a Vendor",
+        advices=slab_AdviceDict["Vendors"]
+    )
+    slab_AdviceGroupDict["Anvil"] = AdviceGroup(
+        tier='',
+        pre_string=f"The following item{pl(slab_AdviceDict['Anvil'])} could be crafted at the Anvil",
+        advices=slab_AdviceDict["Anvil"]
     )
 
     # Generate AdviceSection
@@ -68,6 +106,6 @@ def setSlabProgressionTier():
     if len(slab_AdviceSection.groups) == 0:
         slab_AdviceSection.header = f"All currently owned items registered in The Slab<br>You best ❤️"
     else:
-        slab_AdviceSection.header = f"You're missing some free Slab count!"
+        slab_AdviceSection.header = f"You're missing some easy Slab stacks!"
 
     return slab_AdviceSection
