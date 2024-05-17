f29f4b5da9b99725b8bd0619754d20c409a9b610
Niko Brajdić <niko.brajdic@diversys.solutions>
2024-05-17 13:47:14 +0200

    remember and reuse player-submitted form data

--- mysite/static/scripts/main.js
+++ mysite/static/scripts/main.js
@@ -17,6 +17,22 @@ document.addEventListener("keydown", (e) => {
     if (escPressed) toggleSidebar()
 })
 
+function openSidebarIfFirstAccess() {
+    // show sidebar if opening the page for the first time
+    if (document.querySelectorAll('main, .error').length === 0) {
+        toggleSidebar()
+    }
+}
+
+function defineFormSubmitAction() {
+    document.querySelector('form').addEventListener('submit', (e) => {
+        e.preventDefault()
+        storeUserParams(Object.fromEntries(new FormData(e.target)))
+        const data = fetchStoredUserParams()
+        fetchPlayerAdvice(data)
+    })
+}
+
 // calculate progress bars
 function calcProgressBars(parent = document) {
     const top = el => el.getBoundingClientRect().top
@@ -80,7 +96,8 @@ window.addEventListener('resize', () => {
         resizeTimer = null
     }, 100)
 })
-document.addEventListener("DOMContentLoaded", () => {
+
+function setupFolding() {
     // set event listeners for folding worlds and sections
     document.querySelectorAll('.toggler').forEach(toggler => toggler.onclick = (e) => {
         let title = e.currentTarget
@@ -88,20 +105,27 @@ document.addEventListener("DOMContentLoaded", () => {
             element.classList.toggle("folded")
         }
     })
+}
 
+function setupLightSwitch() {
     document.querySelector('#light-switch').onclick = (e) => {
         document.documentElement.classList.toggle('light-mode')
         e.currentTarget.classList.toggle('on')
         e.currentTarget.classList.toggle('off')
     }
-    document.querySelector('#drawer-handle').onclick = toggleSidebar
+}
 
+function setupSidebarToggling() {
+    document.querySelector('#drawer-handle').onclick = toggleSidebar
+}
+
+function setupSubmitKeybind() {
     // submit the form content if the text area is focused and (Ctrl|Cmd) + Enter is pressed
     document.querySelector("textarea[name='player']").addEventListener("keypress", e => {
         let ctrlCmdPressed = e.ctrlKey || e.metaKey
         let enterPressed = e.code === "Enter"
 
-        if (! (ctrlCmdPressed && enterPressed)) return
+        if (!(ctrlCmdPressed && enterPressed)) return
 
         let clickEvent = new MouseEvent('click', {
             'view': window,
@@ -110,7 +134,9 @@ document.addEventListener("DOMContentLoaded", () => {
         });
         document.querySelector('input[type="submit"]').dispatchEvent(clickEvent);
     })
+}
 
+function setupColorScheme() {
     const runColorMode = (fn) => {
         if (!window.matchMedia) return
         const query = window.matchMedia('(prefers-color-scheme: light)')
@@ -125,29 +151,31 @@ document.addEventListener("DOMContentLoaded", () => {
         isLightMode ? body.add(cls) : body.remove(cls)
         lightSwitch.classList.add(isLightMode ? 'on' : 'off')
     })
+}
 
-    // show sidebar if opening the page for the first time
-    if (document.querySelectorAll('main, .error').length === 0) {
-        toggleSidebar()
-    }
-
+function setupSwitchesActions() {
     // change colour and position of switches when clicked
-    document.querySelectorAll('#switchbox label').forEach(label => label.onclick = e => {
+    document.querySelectorAll('.slider').forEach(label => label.onclick = e => {
         const lbl = e.currentTarget
         const shaft = lbl.querySelector(".shaft")
         shaft.classList.toggle("on")
         shaft.classList.toggle("off")
         const checkbox = lbl.previousElementSibling
         checkbox.value = checkbox.value === "on" ? "off" : "on"
+
+        localStorage.setItem(e.currentTarget.getAttribute("for"), checkbox.value)
     })
 
     // handle left/right handedness switching
-    document.querySelector('#handedness').onclick = () => document.querySelectorAll('.slider, .nav-links, #drawer-handle').forEach(s => s.classList.toggle('lefty'))
+    document.querySelector('#handedness').onclick = () => {
+        document.querySelectorAll('.slider, .nav-links, #drawer-handle').forEach(s => s.classList.toggle('lefty'))
+    }
 
     // toggle progress bars
     document.querySelector('#progress_bars').onclick = () => calcProgressBars(document)
+}
 
-
+function setupPinchyHrefActions() {
     document.querySelectorAll('#pinchy .advice-group a').forEach(hyperlink => hyperlink.onclick = e => {
         const link = e.currentTarget
         const targetId = link.getAttribute("href").slice(1)
@@ -155,6 +183,9 @@ document.addEventListener("DOMContentLoaded", () => {
         target.parentElement.classList.remove('folded')
         target.querySelectorAll('*:not(.empty)').forEach(c => c.classList.remove('folded'))
     })
+}
+
+function applyShowMoreButton() {
     const expandableSections = document.querySelectorAll("#gem-shop .advice-section, #greenstacks .advice-section, #cards .advice-section")
     expandableSections.forEach(section => {
         const expandableGroups = section.querySelector(".groups")
@@ -174,7 +205,9 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
     })
+}
 
+function setupToggleAllAction() {
     document.querySelector("#expand-collapse").onclick = e => {
         const button = e.currentTarget
         button.classList.toggle("closed")
@@ -185,9 +218,12 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         })
     }
+}
 
-    // data age clock
-    setInterval(() => {
+let clockTick
+function setupDataClock() {
+    clearInterval(clockTick)
+    clockTick = setInterval(() => {
         const elapsed = document.querySelector('#elapsed')
         const timeStr = elapsed.innerText
         const [sec, min, hr, d] = timeStr.split(":").reverse()
@@ -211,7 +247,85 @@ document.addEventListener("DOMContentLoaded", () => {
             _sec.toString().padStart(2, "0")
         ].join(":")
     }, 1000)
+}
 
-    // add progress bars
+function setFormValues() {
+    const form = document.querySelector('form')
+    const userParams = fetchStoredUserParams()
+
+    Object.entries(defaults).forEach(([k, v]) => {
+        const userValue = userParams[k] || v
+        const input = form.querySelector(`[name=${k}]`)
+        if (k === "player")
+            input.innerText = userValue
+        else if (input.value.toString() !== userValue)
+            form.querySelector(`[for=${k}]`).click()
+    })
+}
+
+function fetchPlayerAdvice(currentParams) {
+    fetch("/results", {
+        method: 'POST', // Specify the method
+        headers: {
+            'Content-Type': 'application/json' // Set the content type to JSON
+        },
+        body: JSON.stringify(currentParams) // Convert the object to a JSON string
+
+    }).then(response => {
+        initBaseUI()
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.text(); // Get the response as text (HTML)
+
+    }).then(html => {
+        // Find the element where the HTML should be inserted
+        const mainWrapper = document.getElementById('top'); // Adjust this to your target element
+        mainWrapper.innerHTML = html; // Insert the HTML content
+        if (mainWrapper.children.length === 0) return
+        initResultsUI()
+
+    }).catch(error => {
+        console.error('Error:', error); // Handle any errors
+    });
+}
+
+const defaults = {
+    player: "",
+    autoloot: "off",
+    sheepie: "off",
+    doot: "off",
+    order_tiers: "off",
+    progress_bars: "off",
+    handedness: "off"
+}
+
+const storeUserParams = (data) => Object
+    .entries(defaults)
+    .forEach(([k, v]) => localStorage.setItem(k, data[k] || v))
+
+const fetchStoredUserParams = () => Object.fromEntries(Object.entries(defaults)
+    .map(([k, v]) => [k, localStorage.getItem(k) || v]))
+
+document.addEventListener("DOMContentLoaded", () => fetchPlayerAdvice(fetchStoredUserParams()))
+
+function initBaseUI() {
+    openSidebarIfFirstAccess()
+    defineFormSubmitAction()
+    setupLightSwitch()
+    setupSidebarToggling()
+    setupSubmitKeybind()
+    setupColorScheme()
+    setupToggleAllAction()
+    setupSwitchesActions()
+}
+
+function initResultsUI() {
+    setFormValues()
+    toggleSidebar()
+    setupFolding()
+    setupPinchyHrefActions()
+    applyShowMoreButton()
+    setupDataClock()
     calcProgressBars(document)
-});
+}
