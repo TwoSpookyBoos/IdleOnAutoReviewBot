83dfadedb865ad5a01a40c8b2d3529c1f6952098
TwoSpookyBoos <twospookyboos@gmail.com>
2024-06-24 10:07:57 -0500

    Collider Cost Reductions

--- mysite/w3/collider.py
+++ mysite/w3/collider.py
@@ -67,6 +67,90 @@ def getAtomsAdviceGroup() -> AdviceGroup:
     )
     return atoms_ag
 
+def getCostReductionAdviceGroup() -> AdviceGroup:
+    cr_advice = []
+
+    sum_cost_reduction = (1 +
+                          (
+                            7 * session_data.account.merits[4][6]['Level']
+                            + ((session_data.account.construction_buildings['Atom Collider']-1) // 10)
+                            + 1 * session_data.account.atom_collider['Atoms']["Neon - Damage N' Cheapener"]['Level']
+                            + 10 * session_data.account.gaming['SuperBits']['Atom Redux']['Unlocked']
+                            + session_data.account.alchemy_bubbles['Atom Split']['BaseValue']
+                            + session_data.account.stamps['Atomic Stamp']['Value']
+                          )
+                          / 100
+                          )
+
+    cr_advice.append(Advice(
+        label=f"W5 Taskboard Merit: {session_data.account.merits[4][6]['Level'] * 7}%<br>The in-game display is still wrong after a year. Blame Lava.",
+        picture_class='merit-4-6',
+        progression=session_data.account.merits[4][6]['Level'],
+        goal=4
+    ))
+
+    cr_advice.append(Advice(
+        label=f"""Neon - Damage N' Cheapener: {session_data.account.atom_collider['Atoms']["Neon - Damage N' Cheapener"]['Level']}%""",
+        picture_class="neon",
+        progression=session_data.account.atom_collider['Atoms']["Neon - Damage N' Cheapener"]['Level'],
+        goal=20 + (10 * session_data.account.gaming['SuperBits']['Isotope Discovery']['Unlocked'])
+    ))
+
+    cr_advice.append(Advice(
+        label=f"Superbit: Atom Redux: {10 * session_data.account.gaming['SuperBits']['Atom Redux']['Unlocked']}%",
+        picture_class="red-bits",
+        progression=1 if session_data.account.gaming['SuperBits']['Atom Redux']['Unlocked'] else 0,
+        goal=1
+    ))
+
+    cr_advice.append(Advice(
+        label=f"Atom Collider building: {((session_data.account.construction_buildings['Atom Collider']-1) // 10)}%",
+        picture_class="atom-collider",
+        progression=session_data.account.construction_buildings['Atom Collider'],
+        goal=191
+    ))
+
+    cr_advice.append(Advice(
+        label=f"Atom Split bubble: {session_data.account.alchemy_bubbles['Atom Split']['BaseValue']:.2f}%",
+        picture_class="atom-split",
+        progression=session_data.account.alchemy_bubbles['Atom Split']['Level'],
+        resource=session_data.account.alchemy_bubbles['Atom Split']['Material']
+    ))
+
+    cr_advice.append(Advice(
+        label=f"Atomic Stamp: {session_data.account.stamps['Atomic Stamp']['Value']:.3f}%",
+        picture_class="atomic-stamp",
+        progression=session_data.account.stamps['Atomic Stamp']['Level'],
+        resource=session_data.account.stamps['Atomic Stamp']['Material'],
+    ))
+
+    # cr_advice.append(Advice(
+    #     label=f"Total cost reduction: {sum_cost_reduction-1:.2f}",
+    #     picture_class='particles',
+    # ))
+    #
+    # cr_advice.append(Advice(
+    #     label=f"Total cost divisor: {sum_cost_reduction:.2f}",
+    #     picture_class='particles',
+    # ))
+
+    cr_advice.append(Advice(
+        label=f"Remaining cost: {(1/sum_cost_reduction)*100:.2f}%",
+        picture_class='particles',
+    ))
+
+    cr_advice.append(Advice(
+        label=f"Total cost discount: {(1 - (1 / sum_cost_reduction)) * 100:.2f}% off",
+        picture_class='particles',
+    ))
+
+    cr_ag = AdviceGroup(
+        tier="",
+        pre_string="Info- Sources of Atom Collider Cost Reduction",
+        advices=cr_advice
+    )
+    return cr_ag
+
 def setColliderProgressionTier() -> AdviceSection:
     collider_AdviceDict = {}
     collider_AdviceGroupDict = {}
@@ -91,6 +175,7 @@ def setColliderProgressionTier() -> AdviceSection:
     # Generate AdviceGroups
     collider_AdviceGroupDict['ColliderSettings'] = getColliderSettingsAdviceGroup()
     collider_AdviceGroupDict['AllAtoms'] = getAtomsAdviceGroup()
+    collider_AdviceGroupDict['CostReduction'] = getCostReductionAdviceGroup()
 
     # Generate AdviceSection
     overall_ColliderTier = min(max_tier, tier_atomLevels)  # Looks silly, but may get more evaluations in the future
