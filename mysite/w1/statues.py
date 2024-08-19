77ce5b952cfc944b86995cb12347716a36b54fb5
TwoSpookyBoos <twospookyboos@gmail.com>
2024-08-19 09:37:55 -0500

    Alert if able to Onyx statue

--- mysite/w1/statues.py
+++ mysite/w1/statues.py
@@ -4,38 +4,9 @@ from utils.data_formatting import mark_advice_completed
 from utils.logging import get_logger
 from flask import g as session_data
 
+from utils.text_formatting import pl
+
 logger = get_logger(__name__)
-
-def getStatueTypeAdviceGroup() -> AdviceGroup:
-    type_AdviceList = []
-    depositable_statues = 0
-    for statueName, statueDetails in session_data.account.statues.items():
-        if statueDetails['Type'] != statueTypeList[-1]:
-            type_AdviceList.append(Advice(
-                label=f"{statueName}: {statueDetails['Farmer']}",  #: {statueDetails['Type']}",
-                picture_class=statueName,
-                progression=session_data.account.assets.get(statueDetails['ItemName']).amount,
-                goal=20000,
-                resource=statueDetails['Target']
-            ))
-            if session_data.account.assets.get(statueDetails['ItemName']).amount >= 20000:  # and statueDetails['Type'] != statueTypeList[-1]:
-                depositable_statues += 1
-
-    if depositable_statues > 0:
-        session_data.account.alerts_AdviceDict['World 1'].append(Advice(
-            label=f"You can upgrade {depositable_statues} {{{{ Statues|#statues }}}} to {statueTypeList[-1]}!",
-            picture_class="town-marble"
-        ))
-
-    for advice in type_AdviceList:
-        mark_advice_completed(advice)
-
-    type_AG = AdviceGroup(
-        tier="",
-        pre_string=f"Raise all statues to {statueTypeList[-1]}",
-        advices=type_AdviceList
-    )
-    return type_AG
 
 
 def getPreOnyxAdviceGroup() -> AdviceGroup:
@@ -205,6 +176,7 @@ def setStatuesProgressionTier() -> AdviceSection:
     infoTiers = 0
     max_tier = max(statues_progressionTiers.keys()) - infoTiers
     tier_Statues = 0
+    depositable_statues = 0
 
     # Generate Advice
     for tierNumber, tierRequirements in statues_progressionTiers.items():
@@ -236,8 +208,17 @@ def setStatuesProgressionTier() -> AdviceSection:
                             goal=tierRequirements.get('MinStatueTypeNumber', 0) if statueDetails['TypeNumber'] < 1 else 20000,
                             resource=farmResource
                         ))
+                    if session_data.account.assets.get(statueDetails['ItemName']).amount >= 20000 and statueDetails['Type'] != statueTypeList[-1]:
+                        depositable_statues += 1
         if subgroupName not in statues_AdviceDict["Tiers"] and tier_Statues == tierNumber-1:
             tier_Statues = tierNumber
+
+    # Generate Alerts
+    if depositable_statues > 0 and session_data.account.onyx_statues_unlocked:
+        session_data.account.alerts_AdviceDict['World 1'].append(Advice(
+            label=f"You can upgrade {depositable_statues} {{{{ Statue{pl(depositable_statues)}|#statues }}}} to {statueTypeList[-1]}!",
+            picture_class="town-marble"
+        ))
 
     # Generate AdviceGroups
     if session_data.account.maxed_statues < statueCount:
