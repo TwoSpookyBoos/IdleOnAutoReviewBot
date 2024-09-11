293ebd0998eb2ee70fa3973fad240e60e450fed8
TwoSpookyBoos <twospookyboos@gmail.com>
2024-09-11 14:53:35 -0500

    Islands Section Part1
    * Bugfix: Move Steve Sword from T14 to T13

--- mysite/consts.py
+++ mysite/consts.py
@@ -521,7 +521,7 @@ stamps_progressionTiers = {
         "Optional": ["Saw Stamp", "Agile Stamp", "Book Stamp", "Smart Dirt Stamp", "High IQ Lumber Stamp", "Fishhead Stamp", "Fly Intel Stamp", "Talent II Stamp"]
     }},
     13: {"TotalStampLevels": 2500, "Stamps": {
-        "Combat": ["Avast Yar Stamp", "Blackheart Stamp"],
+        "Combat": ["Avast Yar Stamp", "Blackheart Stamp", "Steve Sword",],
         "Skill": ["Banked Pts Stamp", "Nest Eggs Stamp", "Lab Tube Stamp", "Ladle Stamp", "Sailboat Stamp"],
         "Misc": ["Refinery", "Golden Apple Stamp", "Talent III Stamp"],
         "Specific": {
@@ -532,7 +532,7 @@ stamps_progressionTiers = {
         "Optional": ["Arcane Stamp", "Gilded Axe Stamp", "DNA Stamp"]
     }},
     14: {"TotalStampLevels": 4000, "Stamps": {
-        "Combat": ["Steve Sword", "Diamond Axe Stamp", "Sashe Sidestamp"],
+        "Combat": ["Diamond Axe Stamp", "Sashe Sidestamp"],
         "Skill": ["Gamejoy Stamp", "Divine Stamp"],
         "Misc": ["Potion Stamp", "Questin Stamp"],
         "Specific": {
@@ -1064,6 +1064,13 @@ sigils_progressionTiers = {
             "Wizardly Hat": 3, "Metal Exterior": 3, "VIP Parchment": 3, "Tuft Of Hair": 3,
         },
     },
+}
+islands_progressionTiers = {
+    0: {},
+    1: {'Islands': ['Trash Island']},
+    2: {'Islands': ['Seasalt Island', 'Shimmer Island']},
+    3: {'Islands': ['Crystal Island', 'Rando Island']},
+    4: {'Islands': ['Fractal Island']},
 }
 
 ###WORLD 3 PROGRESSION TIERS###
@@ -4528,6 +4535,33 @@ obolsDict = {
 ignorable_obols_list = [
     'Blank', 'LockedInvSpace', 'ObolLocked1', 'ObolLocked2', 'ObolLocked3', 'ObolLocked4',
 ]
+islands_dict = {
+    'Trash Island':     {'Code': '_', 'Description': 'Trade garbage that washs up each day for items',},
+    'Rando Island':     {'Code': 'a', 'Description': 'Guaranteed Random Event once a week',},
+    'Crystal Island':   {'Code': 'b', 'Description': 'Fight daily giant crystal mobs that drop candy',},
+    'Seasalt Island':   {'Code': 'c', 'Description': 'Catch legendary fish for crafting World 6 equips',},
+    'Shimmer Island':   {'Code': 'd', 'Description': 'Do Weekly Challenges for Shimmer Upgrades',},
+    'Fractal Island':   {'Code': 'e', 'Description': 'Dump your time candy here for... nothing...?',},
+}
+islands_trash_shop_costs = {
+    'Skelefish Stamp': 20,
+    'Amplestample Stamp': 40,
+    'Golden Sixes Stamp': 80,
+    'Stat Wallstreet Stamp': 300,
+    'Unlock New Bribe Set': 135,
+    'Filthy Damage Special Talent Book': 450,
+    'Trash Tuna Nametag': 1500
+}
+islands_fractal_rewards_dict = {
+    24: {'Reward': '1 in 100000 chance for Master of Nothing Trophy per hour of Nothing AFK', 'Image': 'master-of-nothing'},
+    200: {'Reward': '1.25x Dungeon Credits and Flurbos gained', 'Image': 'ballot-14'},
+    750: {'Reward': '-30% Kitchen Upgrade Costs', 'Image': 'cooking-table'},
+    2500: {'Reward': '1.20x Chance to find Sailing Artifacts', 'Image': 'artifact-find-chance'},
+    10e3: {'Reward': 'Dirty Shovel digs up +25% more Gold Nuggets', 'Image': 'dirty-shovel'},
+    20e3: {'Reward': '+100 Star Talent Pts', 'Image': 'star-talent-icon'},
+    40e3: {'Reward': 'World 6 Bonus... I wonder what it will be...', 'Image': 'rift-tbd'},
+    60e3: {'Reward': 'World 7 Bonus... I wonder what it will be...', 'Image': 'rift-tbd'},
+}
 
 def getReadableVialNames(inputNumber):
     try:
--- mysite/models/models.py
+++ mysite/models/models.py
@@ -34,6 +34,7 @@ from consts import (
     ballotDict,
     fishingToolkitDict,
     obolsDict, ignorable_obols_list,
+    islands_dict, islands_trash_shop_costs,
     # W3
     buildingsDict, shrinesList, saltLickList, atomsList, colliderStorageLimitList,
     prayersDict,
@@ -1468,6 +1469,7 @@ class Account:
         self._parse_w2_arcade()
         self._parse_w2_ballot()
         self._parse_w2_obols()
+        self._parse_w2_islands()
 
     def _parse_w2_vials(self):
         self.alchemy_vials = {}
@@ -1707,6 +1709,20 @@ class Account:
                 else:
                     self.obols[obolBonusType][obolShape][obol]['Count'] += 1
         #print(f"session_data.account.obols: {self.obols['Drop Rate']}")
+
+    def _parse_w2_islands(self):
+        self.islands = {
+            'Trash': int(float(self.raw_optlacc_dict.get(161, 0))),  #[161]: 362.202271805249
+            'Bottles': int(float(self.raw_optlacc_dict.get(162, 0))),  #[162]: 106.90044163281846
+        }
+        raw_islands_list = list(self.raw_optlacc_dict.get(169, ''))  #[169]: "_dcabe"
+        for islandName, islandData in islands_dict.items():
+            self.islands[islandName] = {
+                'Unlocked': islandData['Code'] in raw_islands_list,
+                'Description': islandData['Description']
+            }
+
+        self.nothing_hours = self.raw_optlacc_dict.get(184, 0)
 
     def _parse_w3(self):
         self._parse_w3_buildings()
@@ -2845,6 +2861,7 @@ class Account:
         self._calculate_w2_sigils()
         self._calculate_w2_cauldrons()
         self._calculate_w2_ballot()
+        self._calculate_w2_islands_trash()
 
     def _calculate_w2_cauldrons(self):
         perCauldronBubblesUnlocked = [
@@ -2900,6 +2917,20 @@ class Account:
             # Check for multi replacements
             if "}" in buffValuesDict['Description']:
                 self.ballot['Buffs'][buffIndex]['Description'] = buffValuesDict['Description'].replace("}", f"{1 + (self.ballot['Buffs'][buffIndex]['Value'] / 100):.3f}")
+
+    def _calculate_w2_islands_trash(self):
+        for item in islands_trash_shop_costs:
+            self.islands['Trash Island'][item] = {'Cost': islands_trash_shop_costs[item]}
+        #Onetime purchases
+        self.islands['Trash Island']['Skelefish Stamp']['Unlocked'] = self.stamps['Skelefish Stamp']['Delivered'] or self.stored_assets.get('StampB47').amount > 0
+        self.islands['Trash Island']['Amplestample Stamp']['Unlocked'] = self.stamps['Amplestample Stamp']['Delivered'] or self.stored_assets.get('StampB32').amount > 0
+        self.islands['Trash Island']['Golden Sixes Stamp']['Unlocked'] = self.stamps['Golden Sixes Stamp']['Delivered'] or self.stored_assets.get('StampA38').amount > 0
+        self.islands['Trash Island']['Stat Wallstreet Stamp']['Unlocked'] = self.stamps['Stat Wallstreet Stamp']['Delivered'] or self.stored_assets.get('StampA39').amount > 0
+        self.islands['Trash Island']['Unlock New Bribe Set']['Unlocked'] = self.bribes['Trash Island']['The Art of the Bail'] >= 0
+
+        #Repeated purchases
+        self.islands['Trash Island']['Garbage Purchases'] = self.raw_optlacc_dict.get(163, 0)
+        self.islands['Trash Island']['Bottle Purchases'] = self.raw_optlacc_dict.get(164, 0)
 
     def _calculate_w3(self):
         self._calculate_w3_building_max_levels()
