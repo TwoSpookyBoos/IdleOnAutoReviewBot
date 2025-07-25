09e1a19bf5d85025fdd49b34810b2f99511ce93f
CallMeHein <heinrichmakovenko@gmail.com>
2025-07-25 18:33:26 +0200

    (Maybe temporary?) Add script to check for references to /wiki or /data files that don't exist

---
+++ check_for_missing_files.py
@@ -0,0 +1,24 @@
+import re
+import os
+from urllib.parse import unquote
+
+wiki_pattern = re.compile(r'wiki/[^"\')\s]+?\.png')
+data_pattern = re.compile(r'data/[^"\')\s]+?\.png')
+
+root_dir = './mysite'
+image_dir = './mysite/static/imgs'
+
+USE_PATTERN = wiki_pattern
+
+for dirpath, _, filenames in os.walk(root_dir):
+    for filename in filenames:
+        file_path = os.path.join(dirpath, filename)
+        try:
+            with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+                for line in f:
+                    for match in USE_PATTERN.findall(line):
+                        decoded_match = unquote(match)
+                        if not os.path.isfile(os.path.join(image_dir, decoded_match)):
+                            print(f'MISSING: {decoded_match} in {file_path}')
+        except (UnicodeDecodeError, PermissionError):
+            continue
