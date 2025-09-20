4a1e6833198aaa277fabb2f310d3d3e12515c731
CallMeHein <heinrichmakovenko@gmail.com>
2025-09-20 20:17:32 +0200

    Make parse_number safer

--- mysite/utils/number_formatting.py
+++ mysite/utils/number_formatting.py
@@ -1,7 +1,15 @@
+from utils.logging import get_logger
 
-def parse_number(number: str) -> int | float:
+logger = get_logger(__name__)
+
+
+def parse_number(number: str, default=1e100) -> int | float:
     """
     Parses a string to a number. Simplifies to an int if possible. Otherwise, it returns a float
     """
-    num = float(number)
-    return int(num) if int(num) == num else num
+    try:
+        num = float(number)
+        return int(num) if int(num) == num else num
+    except:
+        logger.exception(f"An error occurred during parse_number. Replacing with default: {default}")
+        return int(float(default))
