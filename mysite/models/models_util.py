a6435e6e029ed9c4b6297a6bb8c3de1add2b248e
TwoSpookyBoos <twospookyboos@gmail.com>
2025-11-04 21:54:14 -0600

    f-string fix

--- mysite/models/models_util.py
+++ mysite/models/models_util.py
@@ -16,7 +16,7 @@ def get_guild_bonus_advice(bonus_name: str) -> Advice:
 
 def get_upgrade_vault_advice(upgrade_name: str, link_to_section: bool = True, additional_info_text: str = "") -> Advice:
     upgrade = session_data.account.vault['Upgrades'][upgrade_name]
-    main_line = f"{f"{{{{ Upgrade Vault|#upgrade-vault }}}} - {upgrade_name}" if link_to_section else upgrade_name}: {upgrade['Description']}"
+    main_line = f"""{f"{{{{ Upgrade Vault|#upgrade-vault }}}} - {upgrade_name}" if link_to_section else upgrade_name}: {upgrade['Description']}"""
     unlock_line = f"<br>Requires {upgrade['Unlock Requirement'] - session_data.account.vault['Total Upgrades']} more Upgrades to unlock" if not upgrade['Unlocked'] else ""
     return Advice(
         label=main_line + unlock_line + additional_info_text,
