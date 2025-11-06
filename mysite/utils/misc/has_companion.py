996d9db8f07a38bf1971a232c9e3f8ed4010150a
CallMeHein <37670876+CallMeHein@users.noreply.github.com>
2025-11-06 11:58:05 +0100

    throw error when unknown companion is queried

--- mysite/utils/misc/has_companion.py
+++ mysite/utils/misc/has_companion.py
@@ -1,4 +1,7 @@
 from flask import g as session_data
+from consts.consts_idleon import companions_data
 
 def has_companion(companion_name: str) -> bool:
+    if companion_name not in companions_data:
+        raise ValueError(f"Querying unknown companion {companion_name}")
     return companion_name in session_data.account.companions
