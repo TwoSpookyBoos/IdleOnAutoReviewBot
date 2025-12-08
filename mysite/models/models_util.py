fed925be6fb5fc5b223da9b66ca967a874d53213
CallMeHein <37670876+CallMeHein@users.noreply.github.com>
2025-12-08 18:13:13 +0100

    add generator for Event Shop advice. Not fully done yet but it's a start

--- mysite/models/models_util.py
+++ mysite/models/models_util.py
@@ -143,3 +143,15 @@ def get_coral_reef_advice(coral_name: str) -> Advice:
         resource='coral',
     )
     return advice
+
+def get_event_shop_advice(bonus_name: str) -> Advice:
+    # TODO: add "Value" field to Event Shop Items so we can use this in other sections. Currently this generator is only useful for the Event Shop section
+    event_points_total = session_data.account.event_points_shop['Points Owned'] + session_data.account.all_assets.get("Quest89").amount
+    bonus = session_data.account.event_points_shop['Bonuses'][bonus_name]
+    return Advice(
+        label=f"{bonus_name}: {bonus['Description']}",
+        picture_class=bonus['Image'],
+        progression=1 if bonus['Owned'] else event_points_total,
+        goal=1 if bonus['Owned'] else bonus['Cost'],
+        resource='event-point' if not bonus['Owned'] else '',
+    )
