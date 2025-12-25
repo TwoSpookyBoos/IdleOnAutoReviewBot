08f34a521e3529c5b10db8c67eb77b3cdcd24b47
MrSuperKing143 <MrSuperKing143@MyIDKNet.com>
2025-12-26 01:45:17 +1300

    Add level check to Coral Reef Next level costs advice

--- mysite/models/models_util.py
+++ mysite/models/models_util.py
@@ -136,7 +136,7 @@ def get_gem_shop_purchase_advice(
 def get_coral_reef_advice(coral_name: str) -> Advice:
     upgrade = session_data.account.coral_reef['Reef Corals'][coral_name]
     unlock_or_upgrade_text = 'Level up' if upgrade['Unlocked'] else "Unlock"
-    next_level_cost_text = f"<br>Next level costs {upgrade['Next Cost']} corals" if upgrade['Unlocked'] else ''
+    next_level_cost_text = f"<br>Next level costs {upgrade['Next Cost']} corals" if upgrade['Unlocked'] and upgrade['Level'] < upgrade['Max Level'] else ''
     advice = Advice(
         label=f"{unlock_or_upgrade_text} {coral_name}: {upgrade['Description']}{next_level_cost_text}",
         picture_class=upgrade['Image'],
