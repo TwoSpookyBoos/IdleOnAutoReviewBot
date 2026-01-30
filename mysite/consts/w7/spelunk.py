cf3d6d472ac03469cd6ad243d378ddf3e7838e68
nevar <YurinVV@ya.ru>
2026-01-30 23:24:18 +0700

    Add Spelunking Lore Chapter Bonuses

---
+++ mysite/consts/w7/spelunk.py
@@ -0,0 +1,20 @@
+# Part of spelunk page item name
+chapter_name = ["The Fear Within", "Decay Surrounds", "This is Gospel", "No Escape"]
+chapter_bonus_img = [
+    "spelunking",
+    "spelunking",
+    "spelunking",
+    "placeholder",
+    "spelunking",
+    "spelunking",
+    "spelunking-tool-0",
+    "spelunking-amber-0",
+    "spelunking-shop-upgrade-4",
+    "spelunking-shop-upgrade-5",
+    "spelunking-shop-upgrade-6",
+    "placeholder",
+    "spelunking-shop-upgrade-4",
+    "spelunking",
+    "spelunking-elixir-0",
+    "spelunking-chapter-1",
+]
--- mysite/models/w7/spelunk.py
+++ mysite/models/w7/spelunk.py
@@ -1,7 +1,12 @@
-from consts.idleon.w7.spelunk import spelunking_cave_list
+from consts.consts_autoreview import EmojiType, ValueToMulti
+from consts.general.common import percent_break_point
+from consts.idleon.lava_func import lava_func
+from consts.idleon.w7.spelunk import spelunking_cave_list, spelunk_chapters
+from consts.w7.spelunk import chapter_name, chapter_bonus_img
 
 from models.advice.advice import Advice
 
+from utils.number_formatting import round_and_trim
 from utils.safer_data_handling import safe_loads, safer_index
 from utils.logging import get_logger
 
@@ -39,6 +44,79 @@ class SpelunkCave:
         )
 
 
+class LoreBonus:
+    def __init__(self, chapter_index: int, index: int, info: dict, level: int):
+        self.level = level
+        self._template = info["Description"]
+        self._image = chapter_bonus_img[index]
+        self._x1 = info["x1"]
+        self._x2 = info["x2"]
+        self._lava_fun = info["LavaFun"]
+        self._min_page = info["MinPage"]
+        self._multi_by_artifact = info["MultiByArtifact"]
+        self.value = 0
+        self._value_per_level = 0
+        self.max_value = None
+        self.resource = f"spelunking-chapter-{chapter_index + 1}"
+
+    def calculate_bonus(self, multi):
+        multi = self._multi_by_artifact * multi
+        self.value = multi * lava_func(self._lava_fun, self.level, self._x1, self._x2)
+        match self._lava_fun:
+            case "decay":
+                self.max_value = self._x1 * multi
+            case "decayMulti":
+                self.max_value = (1 + self._x1) * multi
+            case _:
+                self._value_per_level = self._x1 * multi
+                self.max_value = None
+
+    def get_bonus_advice(self, link_to_section: bool = True) -> Advice:
+        label = ""
+        if link_to_section:
+            label += "{{ Lore Chapters Bonus|#spelunking }}:<br>"
+        value = f"{round_and_trim(self.value)}"
+        if self.max_value is not None:
+            value += f"/{round_and_trim(self.max_value)}"
+        bonus = self._template.replace("{", value).replace("}", value)
+        if not link_to_section:
+            label += f"Level {self.level}: {bonus}"
+            if self.max_value is not None:
+                current_percent = self.value / self.max_value
+                progress = f"{current_percent:.2%}"
+                goal = "100%"
+                for percent in percent_break_point:
+                    if current_percent < percent:
+                        next_level, next_bonus = self._get_percent_info(percent)
+                        label += f"<br>Level {next_level} ({percent:.0%}): "
+                        label += next_bonus
+                        break
+            else:
+                progress = "Linear"
+                goal = EmojiType.INFINITY.value
+                label += f"<br>+{self._value_per_level} per level"
+            label += f"<br>Required pages: {self._min_page}"
+        else:
+            label += bonus
+            progress = self.level
+            goal = EmojiType.INFINITY.value
+        return Advice(
+            label=label,
+            picture_class=self._image,
+            resource=self.resource,
+            progression=progress,
+            goal=goal,
+        )
+
+    def _get_percent_info(self, percent: float) -> tuple[int, str] | None:
+        if percent >= 1.0 or percent < 0 or self.max_value is None:
+            return None
+        next_value = f"{round_and_trim(self.max_value * percent)}"
+        next_bonus = self._template.replace("{", next_value).replace("}", next_value)
+        next_level = int(self._x2 * percent / (1 - percent))
+        return next_level, next_bonus
+
+
 class Spelunk:
     def __init__(self, raw_data: dict):
         spelunk_info = safe_loads(raw_data.get("Spelunk", []))
@@ -50,3 +128,21 @@ class Spelunk:
             cave_state = safer_index(raw_cave_state, index, 0)
             cave = SpelunkCave(index, cave_info, cave_state)
             self.caves[cave.name] = cave
+        self.lore: dict[str, list[LoreBonus]] = {name: [] for name in chapter_name}
+        self._parse_chapter_lore(spelunk_info)
+
+    def _parse_chapter_lore(self, spelunk_info: list):
+        raw_chapter_bonus_level: list[int] = safer_index(spelunk_info, 8, [])
+        for index, chapter in enumerate(spelunk_chapters):
+            name = chapter_name[index]
+            for bonus_index, info in enumerate(chapter):
+                level_index = index * 4 + bonus_index
+                bonus_level = safer_index(raw_chapter_bonus_level, level_index, 0)
+                lore_bonus = LoreBonus(index, level_index, info, bonus_level)
+                self.lore[name].append(lore_bonus)
+
+    def calculate_lore_bonus(self, artifact):
+        self.lore_multi = ValueToMulti(30 * artifact["Level"])
+        for chapter_bonuses in self.lore.values():
+            for bonus in chapter_bonuses:
+                bonus.calculate_bonus(self.lore_multi)
