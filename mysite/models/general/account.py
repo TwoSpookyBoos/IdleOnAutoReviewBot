7fc75a3be4b738d5b08623e3c7a2a798e5693493
nevar <YurinVV@ya.ru>
2026-02-07 15:46:26 +0700

    Add Meritocracy Bonuses information

--- mysite/models/general/account.py
+++ mysite/models/general/account.py
@@ -13,6 +13,7 @@ from models.w6.sneaking import Sneaking
 from models.w7.spelunk import Spelunk
 from models.w7.advice_fish import AdviceFish
 from models.w7.clam_work import ClamWork
+from models.w7.meritocracy import Meritocracy
 from utils.safer_data_handling import safe_loads, safer_get
 from utils.text_formatting import InputType
 from flask import g
@@ -93,6 +94,7 @@ class Account:
         }
         self.advice_fish = AdviceFish(self.raw_data)
         self.clam_work = ClamWork(self.raw_data)
+        self.meritocracy = Meritocracy(self.raw_data)
 
     def add_alert_list(
         self, group_name: str, advice_list: list[Advice | None] | set[Advice | None]
