d4b5c84141e0d9080ceb5dfac0359d2acad4fb29
Adam E. <274301707+AdamEnright@users.noreply.github.com>
2026-09-11 07:49:00 -0700

    Migrate arcade (#560)
    * Migrate arcade
    
    * Read reindeer multi from the companion
    
    * Delete arcade advice generator

--- mysite/w1/smithing.py
+++ mysite/w1/smithing.py
@@ -8,7 +8,6 @@ from consts.consts_autoreview import break_you_best, ValueToMulti, build_subgrou
 from consts.progression_tiers import smithing_progressionTiers, true_max_tiers
 
 from models.advice.generators.general import get_upgrade_vault_advice
-from models.advice.generators.w2 import get_arcade_advice
 
 from utils.misc.add_subgroup_if_available_slot import add_subgroup_if_available_slot
 from utils.safer_data_handling import safer_convert
@@ -79,7 +78,7 @@ def getForgeCapacityAdviceGroup() -> list[AdviceGroup]:
     cap_Advices['Scaling Sources'].append(session_data.account.stamps['Forge Stamp'].get_advice())
 
     #Arcade Bonus 26 gives Forge Ore Capacity
-    cap_Advices['Scaling Sources'].append(get_arcade_advice(26))
+    cap_Advices['Scaling Sources'].append(session_data.account.arcade[26].get_advice())
 
     #Cosmos > IdleOn Majik #2 Beeg Beeg Forge
     majik_beeg_forge = session_data.account.caverns.villagers["Cosmos"].majiks.idleon['Beeg Beeg Forge']
@@ -93,7 +92,7 @@ def getForgeCapacityAdviceGroup() -> list[AdviceGroup]:
         for advice in cap_Advices[group_name]:
             advice.mark_advice_completed()
 
-    groupA = ValueToMulti((session_data.account.arcade[26]['Value'] + (30 * (next(c.getStars() for c in session_data.account.cards if c.name == 'Godshard Ore')+1))))
+    groupA = ValueToMulti((session_data.account.arcade[26].value + (30 * (next(c.getStars() for c in session_data.account.cards if c.name == 'Godshard Ore')+1))))
     groupB = ValueToMulti(session_data.account.stamps['Forge Stamp'].total_value)
     groupC = ValueToMulti(bribe_value + beeg_forge.total_value)
     groupD = ValueToMulti((50 * achievement) + (25 * skill_mastery_bonus_bool))
