fce91781d195fae770a11053be30e5e9183e7377
AdamEnright <274301707+AdamEnright@users.noreply.github.com>
2026-09-13 06:37:55 -0700

    Only enable debug mode when FLASK_DEBUG is set

--- mysite/config.py
+++ mysite/config.py
@@ -13,7 +13,7 @@ mimetypes.add_type("text/javascript", ".js")
 app = Flask(__name__)
 
 app.config.update(dict(
-    DEBUG=True,
+    DEBUG=os.environ.get("FLASK_DEBUG") == "1",
     PROJECT_ROOT=Path(os.path.dirname(os.path.abspath(__file__))),
     IE="https://www.idleonefficiency.com",
     IT="https://idleontoolbox.com",
