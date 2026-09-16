091bc858cd6dd359306089b5ecec0e4b54bb7001
AdamEnright <274301707+AdamEnright@users.noreply.github.com>
2026-09-16 08:08:05 -0700

    Look up item definitions directly instead of scanning on a miss

--- mysite/models/general/item_definitions.py
+++ mysite/models/general/item_definitions.py
@@ -53,9 +53,7 @@ ItemDef = TypeVar("ItemDef", bound=ItemDefinition | StampItemDefinition)
 
 class ItemDefinitions(dict[str, ItemDef]):
     def get_item_from_codename(self, codename: str) -> ItemDef:
-        if codename in self:
-            return self[codename]
-        return next(item for code, item in self.items() if code == codename)
+        return self[codename]
 
     def get_all_stamps(self) -> list[StampItemDefinition]:
         return [item for item in self.values() if isinstance(item, StampItemDefinition)]
